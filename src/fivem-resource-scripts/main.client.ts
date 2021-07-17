8cb0e050f2fd1c632db02471324de82d6fef37a5
Lisa-Philine Tsakiris <lphiline@web.de>
2021-07-17 23:01:54 +0200

    merge

--- src/fivem-resource-scripts/main.client.ts
+++ src/fivem-resource-scripts/main.client.ts
@@ -35,3 +35,57 @@ setTick(() => {
         DisableControlAction(0, 106, nuiActive);
     }
 })
+
+function wait(ms: number) {
+    new Promise(res => setTimeout(res, ms));
+}
+async function spawnCar(model: string) {
+
+    const hash = GetHashKey(model);
+
+      // Request the model and wait until the game has loaded it
+      RequestModel(hash);
+      while (!HasModelLoaded(hash))
+      {
+          await wait(500);
+      }
+  
+      const ped = PlayerPedId();
+  
+      // Get the coordinates of the player's Ped (their character)
+      //@ts-ignore
+      const coords = GetEntityCoords(ped);
+      //const vector = new Vector3(
+      //    coords[0],
+      //    coords[1],
+      //    coords[2]
+      //);
+
+      //const modelFivemJs = new fivemjs.Model(hash);
+      //const vehicle = fivemjs.World.createVehicle(modelFivemJs, vector, GetEntityHeading(ped));
+  
+      // Create a vehicle at the player's position
+      const vehicle = CreateVehicle(hash, coords[0], coords[1], coords[2], GetEntityHeading(ped), true, false);
+  
+      // Set the player into the drivers seat of the vehicle
+      SetPedIntoVehicle(ped, vehicle, -1);
+  
+      // Allow the game engine to clean up the vehicle and model if needed
+      SetEntityAsNoLongerNeeded(vehicle);
+      SetModelAsNoLongerNeeded(model);
+  
+      // Tell the player the car spawned
+      emit('chat:addMessage', {
+          args: [`Woohoo! Enjoy your new ^*${model}!`]
+      });
+}
+
+RegisterNuiCallbackType('button:spawnCar');
+on('__cfx_nui:button:spawnCar', async(data: any) => {
+    if (data.model === 'adder') {
+        spawnCar('adder');
+    }
+    if (data.model === 't20') {
+        spawnCar('t20');
+    }
+});
