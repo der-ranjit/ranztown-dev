65e55e14b2665a8048f55b65985822ee58b06eb4
Ranjit Mevius <ranjit.mevius@outlook.com>
2021-07-18 01:02:03 +0200

    create eventObservables

--- src/nui-app/core/fivemClient.service.ts
+++ src/nui-app/core/fivemClient.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from "@angular/core";
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { fromEvent, Observable } from "rxjs";
+import { filter } from "rxjs/operators";
 
 @Injectable({providedIn: "root"})
 export class FiveMClientService {
     // get parent resource name by call to GetParentResourceName()
     private parentResourceName = "testmenu";
+
+    /* cache observables created for events */
+    private eventObservables = new Map<string, Observable<any>>();
 
     public constructor(private snackBar: MatSnackBar) {
         this.initListeningToClientEvents();
@@ -20,6 +25,15 @@ export class FiveMClientService {
         });
         return result.json();
     }
+
+    public getEventObservable(eventName: string): Observable<any> {
+        if (!this.eventObservables.has(eventName)) {
+            const observable = fromEvent(window, "message").pipe(filter(event => event.type === eventName));
+            this.eventObservables.set(eventName, observable)
+        }
+        return this.eventObservables.get(eventName)!;
+    }
+
 
     private initListeningToClientEvents(): void {
         window.addEventListener("message", event => {
