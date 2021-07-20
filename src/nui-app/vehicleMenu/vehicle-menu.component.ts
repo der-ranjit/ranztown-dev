5d0785e3c99f36c39a073ba6d4bd0f2335d00800
Ranjit Mevius <ranjit.mevius@outlook.com>
2021-07-20 02:07:18 +0200

    add vehicle thumbs

--- src/nui-app/vehicleMenu/vehicle-menu.component.ts
+++ src/nui-app/vehicleMenu/vehicle-menu.component.ts
@@ -8,12 +8,17 @@ import { AppNuiEventsService } from '../core/nuiEvents.service';
     selector: 'nui-app-vehicle-menu',
     template: `
         <mat-form-field appearance="fill" class="vehicleNameField">
-            <mat-label>Enter vehicle name</mat-label>
+            <mat-label>Vehicle name</mat-label>
             <input type="text" #vehicleName matInput>
+            <mat-hint>enter a vehicle name</mat-hint>
         </mat-form-field>
         <button mat-raised-button (click)="handleSpawnCar(vehicleName.value)">Spawn car</button>
         <div class="vehicleList">
-            <button *ngFor="let vehicle of vehicles" mat-raised-button (click)="handleSpawnCar(vehicle.name)">{{vehicle.name}}</button>
+            <div *ngFor="let vehicle of vehicles" class="vehicleItem"
+                [style.backgroundImage]="'url(assets/vehicle_thumbs/' + vehicle.name +'.png)'"
+                (click)="handleSpawnCar(vehicle.name)">
+                <div class="vehicleName">{{vehicle.name}}</div>
+            </div>
         </div>
 `,
     styles: [`
@@ -30,12 +35,35 @@ import { AppNuiEventsService } from '../core/nuiEvents.service';
         .vehicleList {
             display: flex;
             flex-wrap: wrap;
+            overflow-y: auto;
+            justify-content: space-around;
+        }
+        .vehicleItem {
+            position: relative;
+            width: 120px;
+            height: 100px;
+            margin-top: 4px;
+            border: solid 1px black;
+            border-radius: 5px;
+            background-size: contain;
+            background-repeat: no-repeat;
+            background-color: white;
+            background-position: center;
+        }
+        .vehicleName {
+            position: absolute;
+            left: 0;
+            right: 0;
+            bottom: -3px;
+            display: flex;
+            justify-content: center;
+            color: black
         }
 `]
 })
 export class VehicleMenuComponent {
     // TODO large list loads too long
-    public vehicles = [...Object.values(Vehicles)].slice(0,10);
+    public vehicles = [...Object.values(Vehicles)];
 
     constructor(private events: AppNuiEventsService) {
     }
