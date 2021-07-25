069dc0fdbc3955d4ffc507fab010384ff57197a0
Lisa-Philine Tsakiris <lphiline@web.de>
2021-07-25 14:16:08 +0200

    Add Parachute to flying ped

--- src/fivem-scripts/client/MenuControls.ts
+++ src/fivem-scripts/client/MenuControls.ts
@@ -1,5 +1,5 @@
 import * as Cfx from "fivem-js";
-import { Vector3 } from "fivem-js";
+import { Vector3, WeaponHash } from "fivem-js";
 
 import { Callback, Message } from "../../shared/nui-events";
 import { CfxNuiEventsService, NuiCallbackEvents, NuiCallbackListener } from "./NuiEventsService";
@@ -36,6 +36,9 @@ export class MenuControls {
     public async flyHigh(event: Callback.FlyHigh): Promise<void> {
         const playerPosition = Cfx.Game.PlayerPed.Position;
         Cfx.Game.PlayerPed.Position = new Vector3(playerPosition.x, playerPosition.y, playerPosition.z + 100);
+        Cfx.Game.PlayerPed.giveWeapon(WeaponHash.Parachute, 1, false, true);
+        //GiveWeaponToPed(GetPlayerPed(-1), GetHashKey("gadget_parachute"), 1, false, true)
+        //SetPedComponentVariation(GetPlayerPed(-1), 5, 0, 0, 0);
     }
 
     private initControls(): void {
