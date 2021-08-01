75568089c9dcf8b913c17dde83de22b0f9f7cd51
Ranjit Mevius <ranjit.mevius@outlook.com>
2021-08-02 01:54:46 +0200

    fix import path

--- src/angular/nui-app/flyHigh/flyHigh.component.ts
+++ src/angular/nui-app/flyHigh/flyHigh.component.ts
@@ -1,7 +1,8 @@
 import { AnimationEvent } from "@angular/animations";
 import { Component, Input, Output } from "@angular/core";
 import { Subject } from "rxjs";
-import { FlyHigh } from 'src/shared/nui-events/callbacks';
+
+import { FlyHigh } from "../../../shared/nui-events/callbacks";
 import { slideIn } from "../core/animations";
 import { AppNuiEventsService } from "../core/nui-events/nuiEvents.service";
 
