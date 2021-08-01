a70055ac3a9eea15aabc75ee324406e3391d1f48
Ranjit Mevius <ranjit.mevius@outlook.com>
2021-08-01 16:46:51 +0200

    update nui webpack to hopefully be able to mixin copywebpack plugin without breaking angular-cli webpack stuff

--- config/nui-webpack.config.js
+++ config/nui-webpack.config.js
@@ -8,27 +8,35 @@ if (!resourceName) {
     return;
 }
 
+/**
+ * https://github.com/just-jeb/angular-builders/tree/master/packages/custom-webpack#merging-plugins-configuration
+ * because we use a custom angular webpack builder, the webpack config needs to return a function; this way
+ * our custom rules do not overwrite angular-cli own rules and break the app
+ *
+/* we are using a custom webpack angular builder;  */
+module.exports = (config, options, targetOptions) => {
+    if (process.env.NUI_MODE == "dev-server") {
+        /* the dev server does not need to produce an fxmanifest or copy assets */
+        config.push(
+            new DefinePlugin({
+                RESOURCE_NAME: JSON.stringify(resourceName)
+            })
+        );
+    } else {
+        config.plugins.push(
+            new DefinePlugin({
+                RESOURCE_NAME: JSON.stringify(resourceName)
+            }),
+            new ShellPlugin({
+                onBuildEnd: {
+                    scripts: ["npm run nui:copy-assets"]
+                },
+                onAfterDone: {
+                    scripts: ["npm run fivem:create"]
+                }
+            })
+        );
+    }
 
-/* the dev server does not need to produce an fxmanifest or copy assets */
-const devServerPlugins = [
-    new DefinePlugin({
-        RESOURCE_NAME: JSON.stringify(resourceName)
-    })
-];
-
-const plugins = [
-    new DefinePlugin({
-        RESOURCE_NAME: JSON.stringify(resourceName)
-    }),
-    new ShellPlugin({
-        onBuildEnd: {
-            scripts: [ "npm run nui:copy-assets" ]
-        },
-        onAfterDone: {
-            scripts: [ "npm run fivem:create" ]
-        }
-    })
-]
-module.exports = {
-    plugins: process.env.NUI_MODE == "dev-server" ? devServerPlugins : plugins
+    return config;
 }
