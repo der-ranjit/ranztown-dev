9737665e6d3fd824ce079b33a58a220152360ee3
Lisa-Philine Tsakiris <lphiline@web.de>
2021-08-02 23:36:05 +0200

    Add dummy circular component

---
+++ src/angular/nui-app/circularMenu/circular-menu.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+import { slideIn } from '../core/animations';
+
+declare const wheelnav: any;
+
+@Component({
+    selector: 'nui-app-circular-menu',
+    template: `
+        <div class="circularMenuContainer">
+            <div id="wheelDiv" style="width:300px"></div>
+        </div>
+`,
+    styles: [`
+        :host {
+
+        }
+
+    `],
+    animations: [
+        slideIn
+    ]
+})
+export class CircularMenuComponent implements OnInit {
+
+    ngOnInit() {
+
+        // create wheelnav
+        // the div is in the template
+        let wheel = new wheelnav("wheelDiv");
+        wheel.initWheel(["init", "create", "navigate", "refresh"]);
+        wheel.createWheel();
+      }
+}
